d8ca052d73487227aa5c4b9a907e7c188c5a3d74
fsj930210 <fsj930210@163.com>
2023-12-22 00:08:15 +0800

    feat: 增加e2e

--- src/node/dev.ts
+++ src/node/dev.ts
@@ -1,11 +1,17 @@
 import { createServer as createViteDevServer } from 'vite';
 import pluginReact from '@vitejs/plugin-react';
 import { pluginIndexHtml } from './plugins/indexHtml';
+import { PACKAGE_ROOT } from './constants';
 
 // 创建vite devserver
 export async function createDevServer(root = process.cwd()) {
   return createViteDevServer({
     root,
-    plugins: [pluginIndexHtml(), pluginReact()]
+    plugins: [pluginIndexHtml(), pluginReact()],
+    server: {
+      fs: {
+        allow: [PACKAGE_ROOT]
+      }
+    }
   });
 }
