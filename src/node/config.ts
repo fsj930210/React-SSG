2734b87601f378bb881286410f40cb471ebb921c
fsj930210 <fsj930210@163.com>
2023-12-24 15:24:58 +0800

    feat: 通用配置文件解析逻辑开发

---
+++ src/node/config.ts
@@ -0,0 +1,47 @@
+import { resolve } from 'node:path';
+import fs from 'fs-extra';
+import { loadConfigFromFile } from 'vite';
+import { UserConfig } from '../shared/types/index';
+
+type RawConfig = UserConfig | Promise<UserConfig> | (() => UserConfig | Promise<UserConfig>);
+
+export async function resolveConfig(
+  root: string,
+  command: 'serve' | 'build',
+  mode: 'development' | 'production'
+) {
+  // 1. 获取配置文件
+  const configPath = getUserConfigPath(root);
+  // 2. 读取配置文件内容
+  const result = await loadConfigFromFile(
+    {
+      command,
+      mode
+    },
+    configPath,
+    root
+  );
+
+  if (result) {
+    const { config: rawConfig = {} as RawConfig } = result;
+    // 三种情况
+    // 1 object 2. promise 3. function
+    const userConfig = await (typeof rawConfig === 'function' ? rawConfig() : rawConfig);
+    return [configPath, userConfig] as const;
+  } else {
+    return [configPath, {} as UserConfig] as const;
+  }
+}
+
+function getUserConfigPath(root: string) {
+  try {
+    const supportConfigFiles = ['config.ts', 'config.js'];
+    const configPath = supportConfigFiles
+      .map((file) => resolve(root, file))
+      .find(fs.pathExistsSync);
+    return configPath;
+  } catch (error) {
+    console.error(`Failed to load user config: ${error}`);
+    throw error;
+  }
+}
--- src/node/dev.ts
+++ src/node/dev.ts
@@ -2,10 +2,12 @@ import { createServer as createViteDevServer } from 'vite';
 import pluginReact from '@vitejs/plugin-react';
 import { pluginIndexHtml } from './plugins/indexHtml';
 import { PACKAGE_ROOT } from './constants';
+import { resolveConfig } from './config';
 
 // 创建vite devserver
 export async function createDevServer(root = process.cwd()) {
-  console.log(PACKAGE_ROOT, 'PACKAGE_ROOT', root);
+  const config = await resolveConfig(root, 'serve', 'development');
+  console.log(config);
   return createViteDevServer({
     root,
     plugins: [pluginIndexHtml(), pluginReact()],
