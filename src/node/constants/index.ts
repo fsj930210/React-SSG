1a7c7f1bd44c705f1011eb38157b14c1ba170a53
fsj930210 <fsj930210@163.com>
2023-12-30 13:54:54 +0800

    fix: fix mdx hotupdate

--- src/node/constants/index.ts
+++ src/node/constants/index.ts
@@ -14,3 +14,5 @@ export const DEFAULT_HTML_PATH = join(PACKAGE_ROOT, 'template.html');
 export const CLIENT_ENTRY_PATH = join(RUNTIME_PATH, 'ClientEntry.tsx');
 
 export const SERVER_ENTRY_PATH = join(RUNTIME_PATH, 'ServerEntry.tsx');
+
+export const MD_REGEX = /\.mdx?$/;
--- src/node/plugins/mdx/index.ts
+++ src/node/plugins/mdx/index.ts
@@ -1,6 +1,6 @@
 import { Plugin } from 'vite';
 import { pluginMdxRollup } from './pluginMdxRollup';
-
+import { pluginMdxHMR } from './pluginMdxHmr';
 export function createPluginMdx(): Plugin[] {
-  return [pluginMdxRollup()];
+  return [pluginMdxRollup(), pluginMdxHMR()];
 }
